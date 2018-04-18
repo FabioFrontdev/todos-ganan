602f22972ad79a04d6545d26e9f4ac07fdd5ec3e
Carlos Rubio BM <crubio@bmdigital.co>
2018-04-18 10:35:05 -0500

    Header short version

--- views/header.php
+++ views/header.php
@@ -1,4 +1,4 @@
-<!-- <div class="nav">
+<div class="nav">
 	<div class="navShort">
 		<div class="navShortContainer">
 			<div class="u-logo">
@@ -6,6 +6,60 @@
 					<img src="img/icono-logo.png" alt="Todos Ganan">
 				</a>
 			</div>
+			<div class="u-profile">
+				<a href="#">
+					<img src="img/main-profile.svg" alt="Profile">
+				</a>
+			</div>
+			<div class="u-points">
+				<div class="item">
+					<a class="tooltipped u-yellow" data-position="right" data-delay="50" data-tooltip="Puntos por referidos 15">
+						<i class="fa fa-circle-o" aria-hidden="true"></i>
+					</a>
+				</div>
+				<div class="item">
+					<a class="tooltipped u-purple" data-position="right" data-delay="50" data-tooltip="Puntos acumulados 4580">
+						<i class="fa fa-circle-o" aria-hidden="true"></i>
+					</a>
+				</div>
+				<div class="item">
+					<a  class="tooltipped u-red" data-position="right" data-delay="50" data-tooltip="Puntos redimidos 1500">
+						<i class="fa fa-circle-o" aria-hidden="true"></i>
+					</a>
+				</div>
+				<div class="item">
+					<a class="tooltipped u-blue" data-position="right" data-delay="50" data-tooltip="Total 5000">
+						<i class="fa fa-circle-o" aria-hidden="true"></i>
+					</a>
+				</div>
+			</div>
+			<div class="u-menu">
+				<div class="item">
+					<a href="#" class="tooltipped" data-position="right" data-delay="50" data-tooltip="Iniciar sesión">
+						<img src="img/icono-user.svg" align="Iniciar sesión">
+					</a>
+				</div>
+				<div class="item">
+					<a href="#" class="tooltipped" data-position="right" data-delay="50" data-tooltip="Premios">
+						<img src="img/icono-premios.svg" alt="Premios">
+					</a>
+				</div>
+				<div class="item">
+					<a href="#"  class="tooltipped" data-position="right" data-delay="50" data-tooltip="Calendario y resultados">
+						<img src="img/icono-calendario.svg" alt="Calendario y resultados">
+					</a>
+				</div>
+				<div class="item">
+					<a href="#" class="tooltipped" data-position="right" data-delay="50" data-tooltip="Polla Mundialista">
+						<img src="img/icono-polla.svg" alt="Polla Mundialista">
+					</a>
+				</div>
+				<div class="item">
+					<a href="#" class="tooltipped" data-position="right" data-delay="50" data-tooltip="Reglas del Juego">
+						<img src="img/icono-tyc.svg" alt="Reglas del Juego">
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
-</div> -->
+</div>
